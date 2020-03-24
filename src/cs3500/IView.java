49d8e5454bc58c7e0e2002c870a16683c6759438
abagul1 <akhilbagul80@gmail.com>
2020-03-23 20:43:10 -0400

    updated view interface

--- src/cs3500/IView.java
+++ src/cs3500/IView.java
@@ -1,4 +1,13 @@
 package cs3500;
 
 public interface IView {
+  /**
+   * Refresh the view to reflect any changes in the game state.
+   */
+  void refresh();
+
+  /**
+   * Make the view visible to start the game session.
+   */
+  void makeVisible();
 }
