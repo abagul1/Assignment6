3d677650a4242a99cf6e4a475336d2b37ac3c749
dr-ehle <ehle.d@husky.neu.edu>
2020-03-25 20:01:42 -0400

    Adds correct offset to AnimationPanel. Overrides the getPreferredSize() method to return the dimensions that were provided to the model.

--- src/cs3500/views/visualview/AnimationPanel.java
+++ src/cs3500/views/visualview/AnimationPanel.java
@@ -1,5 +1,6 @@
 package cs3500.views.visualview;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
@@ -22,9 +23,15 @@ public class AnimationPanel extends JPanel {
     super.paintComponent(g);
 
     Graphics2D g2d = (Graphics2D) g;
+    g2d.translate(-rom.getX(), -rom.getY());
 
     for (String key : rom.getElements().keySet()) {
       rom.getElement(key).paint(g2d);
     }
+  }
+
+  @Override
+  public Dimension getPreferredSize() {
+    return new Dimension(rom.getWidth(), rom.getHeight());
   }
 }
